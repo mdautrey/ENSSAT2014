8ceaf132ee5da4564aaf332747926d513fff0862
MDY <mikael.dautrey@isitix.com>
2014-10-13 12:17:52 +0200

    corrige tp3'

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -4,17 +4,33 @@ package hello;
  * Created by mdautrey on 28/09/14.
  */
 
+import model.Individu;
 import model.data.InterfaceIndividu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController  {
 
     @Autowired private InterfaceIndividu interfaceIndividu;
+
+    @RequestMapping(value="/addindividu", method= RequestMethod.GET)
+    public String individuForm(Model model) {
+        model.addAttribute("individu", new Individu());
+        return "addindividu";
+    }
+
+
+    @RequestMapping(value="/addindividu", method=RequestMethod.POST)
+    public String greetingSubmit(@ModelAttribute Individu individu, Model model) {
+        model.addAttribute("individu", individu);
+        return "confirmindividu";
+    }
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
